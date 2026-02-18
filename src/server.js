473159e8c8750205ace8fc0ee689c33791ae8a93
Sbouyabri4 <Bouyabri04@gmail.com>
2026-02-18 12:30:22 +0100

    chore: add src structure and app middleware

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Hotel API running ✅" });
 });
 
+// TODO: routes API (plus tard)
+// app.use("/api/chambres", ...)
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
